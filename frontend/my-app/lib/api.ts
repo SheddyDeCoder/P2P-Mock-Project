c4e93d5b54c75b2a6ff54a22c115553954b3a946
Sheddy146 <shedracknliam@gmail.com>
2026-03-13 15:39:52 +0000

    changed frontend api

--- frontend/my-app/lib/api.ts
+++ frontend/my-app/lib/api.ts
@@ -13,7 +13,7 @@ import axios from 'axios';
  * ============================================================
  */
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5005', // Removed /api
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://p2p-mock-project.onrender.com', // Removed /api
   headers: {
     'Content-Type': 'application/json',
   },
