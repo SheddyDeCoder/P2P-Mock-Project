3e4430ee64c11083e0770313d7ba53a3dbfafacf
Paschalbaylon <97518763+Paschalbaylon@users.noreply.github.com>
2026-03-17 20:00:12 +0100

    cors

--- backend/p2-p/src/main.ts
+++ backend/p2-p/src/main.ts
@@ -8,6 +8,16 @@ import { HttpExceptionFilter } from './Exceptions/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://p2p-mock-project-frontend.onrender.com',
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -31,11 +41,14 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://p2p-mock-project-frontend.onrender.com'], // your Next.js port
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000',
+  //     'https://p2p-mock-project-frontend.onrender.com',
+  //   ], // your Next.js port
+  //   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  //   credentials: true,
+  // });
 
   await app.listen(process.env.PORT ?? 5005);
 }
